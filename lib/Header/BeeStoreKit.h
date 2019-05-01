9f751e5a6c5db7d7de5ec60aaba7b7c61a126649
5hito <beemans@foxmail.com>
2019-05-01 09:35:27 +0800

    1.5.3

--- lib/Header/BeeStoreKit.h
+++ lib/Header/BeeStoreKit.h
@@ -18,23 +18,31 @@ typedef NS_ENUM(NSInteger, BeePaymentRequestProductCode) {
 
 @interface BeeStoreKit : NSObject
 
-@property NSArray *availableProducts;
-@property NSMutableDictionary *purchaseRecord;
-
 + (instancetype)sharedKit;
 - (void)validatePurchaseConfigs:(NSDictionary*)configs;
 - (void)startValidateConfigs:(NSDictionary*)configs;
-- (NSString*)getAppStorePurchaseReceiptData;
+
 - (void)startProductRequest;
 - (void)startProductRequestWithProductIdentifiers:(NSArray*)items;
 - (void)restorePurchases;
 - (void)refreshAppStoreReceipt;
+
 - (BeePaymentRequestProductCode)initiatePaymentRequestForProductWithIdentifier:(NSString *)productId;
 - (BOOL)purchasedAppBeforeVersion:(NSString *)requiredVersion;
 - (BOOL)isProductPurchased:(NSString *)productId;
 - (NSDate *)expiryDateForProduct:(NSString *)productId;
+- (NSString*)getAppStorePurchaseReceiptData;
+
 - (NSNumber *)availableCreditsForConsumable:(NSString *)consumableID;
 - (NSNumber *)consumeCredits:(NSNumber *)creditCountToConsume identifiedByConsumableIdentifier:(NSString *)consumableId;
 - (void)setDefaultCredits:(NSNumber *)creditCount forConsumableIdentifier:(NSString *)consumableId;
+
+- (NSNumber*)getPurchaseRecordWithProductIdentifier:(NSString*)identifier;//获取过期时间
+- (NSArray<NSString*>*)getAllKeysByPurchaseRecord;//获取所有购买id
+- (BOOL)isTrialPeriodWithProductIdentifier:(NSString*)identifier;//是否是试用期
+- (BOOL)getProductLocalePriceWithIdentifier:(NSString*)identifier
+                                      price:(NSString**)price
+                               currencyCode:(NSString**)code
+                             currencySymbol:(NSString**)symbol;//获取产品价格及符号
 
 @end
